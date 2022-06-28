26e6a8d87f5ff3aece915f064ce8153c95b48fb9
Nassime MOUNTASIR <n.mountasir@technologyandstrategy.com>
2022-06-28 16:48:03 +0200

    modification des bdd

--- main/bot.py
+++ main/bot.py
@@ -88,8 +88,8 @@ class ChallengesCog(commands.Cog, name='Challenges'):
         cur.execute(""" DROP TABLE IF EXISTS members """)
         con.commit()
         members = [[(i-1), member.id, member.name] for i, member in enumerate(ctx.guild.members) if bot.user.id != member.id]
-        self.bot.challs = pd.DataFrame(members, columns=['id', 'discordid', 'name'])
-        self.bot.challs.to_sql('members', con=engine, index_label='id', if_exists='replace')
+        self.bot.challs = pd.DataFrame(members, columns=['discordid', 'name'])
+        self.bot.challs.to_sql('members', con=engine, if_exists='replace')
 
         query = """ DROP TABLE IF EXISTS challenges """
         cur.execute(query)
