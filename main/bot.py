690214580714ddd1738779cd6d8ccdf2bd5fa8d7
MOUNTASIR NASSIME <nassime.mountasir@etu.unistra.fr>
2022-06-29 12:13:10 +0200

    ajout help nouveau

--- main/bot.py
+++ main/bot.py
@@ -25,6 +25,25 @@ intents.members = True
     
 bot = commands.Bot(command_prefix='!', description=DESCRIPTION, intents=intents)
 
+async def send_embed(ctx, embed):
+    """
+    Function that handles the sending of embeds
+    -> Takes context and embed to send
+    - tries to send embed in channel
+    - tries to send normal message when that fails
+    - tries to send embed private with information abot missing permissions
+    If this all fails: https://youtu.be/dQw4w9WgXcQ
+    """
+    try:
+        await ctx.send(embed=embed)
+    except discord.errors.Forbidden:
+        try:
+            await ctx.send("Erreur d'envoi, merci de vérifier les permissions du bot !")
+        except discord.errors.Forbidden:
+            await ctx.author.send(
+                f"Ah c'est marrant mais je peux pas envoyer de message sur le canal {ctx.channel.name} de {ctx.guild.name}...\n"
+                f"Tu peux le dire à UrFour s'il te plaît ? :slight_smile: ", embed=embed)
+
 @bot.event
 async def on_ready():
     print(f'Connecté en tant que {bot.user} (ID : {bot.user.id})')
@@ -41,6 +60,111 @@ async def on_member_join(member):
     cur.execute(query, (member.id, member.name))
     con.commit()
     print(f"{member.name} a été ajouté à la base de données.")
+
+class Help(commands.Cog):
+    """
+    Sends this help message
+    """
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    # @commands.bot_has_permissions(add_reactions=True,embed_links=True)
+    async def help(self, ctx, *input):
+        """Shows all modules of that bot"""
+	
+	# !SET THOSE VARIABLES TO MAKE THE COG FUNCTIONAL!
+        prefix = '!'
+        version = 'v1'
+        
+        # setting owner name - if you don't wanna be mentioned remove line 49-60 and adjust help text (line 88) 
+        owner = '149195176093548544'
+        owner_name = 'Nassime#5430'
+
+        # checks if cog parameter was given
+        # if not: sending all modules and commands not associated with a cog
+        if not input:
+            # checks if owner is on this server - used to 'tag' owner
+            try:
+                owner = ctx.guild.get_member(owner).mention
+
+            except AttributeError as e:
+                owner = owner
+
+            # starting to build embed
+            emb = discord.Embed(title='Commandes et modules', color=discord.Color.blue(),
+                                description=f'`{prefix}help <module>` pour avoir plus d\'information sur ce module '
+                                            f':smiley:\n')
+
+            # iterating trough cogs, gathering descriptions
+            cogs_desc = ''
+            for cog in self.bot.cogs:
+                cogs_desc += f'`{cog}` {self.bot.cogs[cog].__doc__}\n'
+
+            # adding 'list' of cogs to embed
+            emb.add_field(name='Modules', value=cogs_desc, inline=False)
+
+            # integrating trough uncategorized commands
+            commands_desc = ''
+            for command in self.bot.walk_commands():
+                # if cog not in a cog
+                # listing command if cog name is None and command isn't hidden
+                if not command.cog_name and not command.hidden:
+                    commands_desc += f'{command.name} - {command.help}\n'
+
+            # adding those commands to embed
+            if commands_desc:
+                emb.add_field(name='Autre', value=commands_desc, inline=False)
+
+            # setting information about author
+            emb.add_field(name="A propos", value=f"Bot de la guilde qui monte malgré le silence des médias")
+            emb.set_footer(text=f"Version actuelle du bot : {version}")
+
+        # block called when one cog-name is given
+        # trying to find matching cog and it's commands
+        elif len(input) == 1:
+
+            # iterating trough cogs
+            for cog in self.bot.cogs:
+                # check if cog is the matching one
+                if cog.lower() == input[0].lower():
+
+                    # making title - getting description from doc-string below class
+                    emb = discord.Embed(title=f'{cog} - Commandes', description=self.bot.cogs[cog].__doc__,
+                                        color=discord.Color.green())
+
+                    # getting commands from cog
+                    for command in self.bot.get_cog(cog).get_commands():
+                        # if cog is not hidden
+                        if not command.hidden:
+                            emb.add_field(name=f"`{prefix}{command.name}`", value=command.help, inline=False)
+                    # found cog - breaking loop
+                    break
+
+            # if input not found
+            # yes, for-loops have an else statement, it's called when no 'break' was issued
+            else:
+                emb = discord.Embed(title="wtf",
+                                    description=f"Désolé mais le module `{input[0]}` n'existe pas :scream:",
+                                    color=discord.Color.orange())
+
+        # too many cogs requested - only one at a time allowed
+        elif len(input) > 1:
+            emb = discord.Embed(title="That's too much.",
+                                description="Please request only one module at once :sweat_smile:",
+                                color=discord.Color.orange())
+
+        else:
+            emb = discord.Embed(title="It's a magical place.",
+                                description="I don't know how you got here. But I didn't see this coming at all.\n"
+                                            "Would you please be so kind to report that issue to me on github?\n"
+                                            "https://github.com/nonchris/discord-fury/issues\n"
+                                            "Thank you! ~Chris",
+                                color=discord.Color.red())
+
+        # sending reply embed using our own function defined above
+        await send_embed(ctx, emb)
 
 class ChallengesCog(commands.Cog, name='Challenges'):
     def __init__(self, bot):
@@ -284,4 +408,5 @@ class MiscCog(commands.Cog, name='Divers'):
 if __name__ == "__main__":
     bot.add_cog(ChallengesCog(bot))
     bot.add_cog(MiscCog(bot))
+    bot.add_cog(Help(bot))
     bot.run(TOKEN)
