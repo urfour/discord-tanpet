71b4f930162fa06b219886b48076add5ab5db2e2
MOUNTASIR NASSIME <nassime.mountasir@etu.unistra.fr>
2022-06-29 13:20:41 +0200

    locale fr

--- main/bot.py
+++ main/bot.py
@@ -27,14 +27,7 @@ intents.members = True
 bot = commands.Bot(command_prefix='!', description=DESCRIPTION, intents=intents)
 
 async def send_embed(ctx, embed):
-    """
-    Function that handles the sending of embeds
-    -> Takes context and embed to send
-    - tries to send embed in channel
-    - tries to send normal message when that fails
-    - tries to send embed private with information abot missing permissions
-    If this all fails: https://youtu.be/dQw4w9WgXcQ
-    """
+
     try:
         await ctx.send(embed=embed)
     except discord.errors.Forbidden:
