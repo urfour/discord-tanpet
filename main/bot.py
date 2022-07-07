b94a6a600c613cf4060834a1b11909bff48cc18c
MOUNTASIR NASSIME <nassime.mountasir@etu.unistra.fr>
2022-07-07 20:40:38 +0200

    test challs

--- main/bot.py
+++ main/bot.py
@@ -328,7 +328,7 @@ class ChallengesCog(commands.Cog, name='Challenges'):
             else:
                 embed = discord.Embed(color=discord.Color.orange())
             print(challs)
-            for l in challs[i*25:i*25+1]:
+            for l in challs[i*25:(i+1)*25]:
                 embed.add_field(name=l[0], value=l[1], inline=False)
             await user.send(embed=embed)
 
